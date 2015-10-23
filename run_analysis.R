c7bdaa8115fecb69792d14cd7aa6e805bcdc08dc
Maurizio-Mario <maurizio.murino@hotmail.com>
2015-10-23 12:32:07 +0200

    PA finished. Solved rbinding forced memory allocation

--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,6 @@
 getwd()
-setwd("C:/Users/MaurizioLocale/OneDrive/Data_Science/3 Getting and Cleaning Data/R programming")
-dir()
 
-# 1) Import subject_train. Each row identifies the subject who performed the activity for each window sample. Range 1:30
+# 1) Import subject_train. Each row identifies the subject who performed the activity for each window sample. Range 1:30 but excluded few cases, contained in subject_text.txt. Import it in the "test" merging session. in cul ti e ci t'ha cagà. 
 
 subject_train <- read.table(file = file.path("C:/Users/MaurizioLocale/OneDrive/Data_Science/3 Getting and Cleaning Data/Getting-cleaning-data-Assignments/UCI HAR Dataset/train", "subject_train.txt"), header = TRUE)
 
@@ -40,13 +38,9 @@ gc()
 # 6) Merge join_train_1 from 5) with X_train from 6).
 
 dim(X_train)
-rm(list = "join_train_1", "X_train")
+
 join_train_2 <- cbind(join_train_1, X_train)
-memory.limit(size = 12058)
-dim(join_train_2)
-str(join_train_2)
-head(join_train_2)
-names(join_train_2)
+rm(list = "join_train_1", "X_train")
 
 # 7) Import subject_test. Each row identifies the subject who performed the activity for each window sample. Range 1:30
 
@@ -69,51 +63,54 @@ dim(X_test)
 dim(y_test)
 
 # 11) Cbinding subject_test, X_test, y_test plus deletion
-join_test_2 <- cbind(subject_test, X_test, y_test)
+join_test_2 <- cbind(subject_test, y_test, X_test)
 join_test_2
 dim(join_test_2)
-rm(list = "X_test", "y_test")
+rm(list = "X_test", "y_test", "subject_test")
 
-# 12) Now i have two datasets of 2946*563(join_test_2) and of 7351*563 (join_train_2). Operate rbind to merge them.
+# 12) Step 3  of the Programmin Assignment. Labelling activities in join_train_2$5 and join_test_2$X5
 
-test_train <- rbind(join_train_2, join_train_2)
-dim(test_train)
-str(head(test_train, 1))
+join_train_2$X5 <- factor(join_train_2$X5, levels = c(1:6), labels = c( "walking", "walking_upstair", "walking_downstairs", "sitting", "standing", "laying"))
 
-# 13) Labelling activities values
+join_test_2$X5 <- factor(join_test_2$X5, levels = c(1:6), labels = c( "walking", "walking_upstair", "walking_downstairs", "sitting", "standing", "laying"))
 
-summary(test_train$X5)
-
-test_train$X5 <- factor(test_train$X5, levels = c(1:6), labels = c( "walking", "walking_upstair", "Walking_downstairs", "sitting", "standing", "laying"))
-
-# 14) Labelling every column of test_train
+# 14) Step 4 of the Programming Assignment. Labelling every column of join_test_2 and join_train_2 with descriptive variable names.
 
 var_names <- read.table(file = file.path("C:/Users/MaurizioLocale/OneDrive/Data_Science/3 Getting and Cleaning Data/Getting-cleaning-data-Assignments/UCI HAR Dataset", "features.txt"), header = FALSE)
-        
+
 var_names <- subset(var_names, select = -V1)
 new_rows <- data.frame(V2 = c("subject", "activity"))
 var_names <- rbind(new_rows, var_names)
 as.data.frame(var_names)
-test_train2 <- test_train
-colnames(test_train2) <- var_names$V2 
-names(test_train2)
+colnames(join_test_2) <- var_names$V2
+colnames(join_train_2) <- var_names$V2
 
-# 15) Clean workspace
+# 15) remove var_names and new_rows
 
-rm(list=setdiff(ls(), "test_train2"))
+rm(list = "var_names", "new_rows")
 
-# 16) Extract mean and standard deviation (Step 2 of the course project)
+# 16) Step 1 of the Programming Assignment. Extracts only the measurements on the mean and standard deviation for each measurement. Now i have two tidy and pretty datasets of 2946*563(join_test_2) and of 7351*563 (join_train_2), composed by the "training" and the "test" sets. Operate rbind to merge them and order() to sort them. 
 
-test_train3 <- test_train2[grep("subject|activity|mean|std", colnames(test_train2), value = TRUE)]
-dim(test_train3)
+test_train1 <- rbind(join_train_2, join_test_2)
+test_train1 <- test_train1[order(test_train1$subject, test_train1$activity),]
+dim(test_train)
 
-# 17)
+# 15) Clean workspace exept for the merged dataset of 14).
 
-sum(is.na(test_train3))
-print(test_train3)
-class(test_train3)
-mean(test_train3[3:length(test_train3)])
-tapply(test_train3,test_train3$subject, mean)
-mean(test_train3$`tBodyAcc-mean()-X`)
-aggregate(test_train3[, 3:81], list(test_train3$activity), mean)
+rm(list=setdiff(ls(), "test_train1"))
+
+# 16) Step 2 of the Assignment project. Extracts only the measurements on the mean and standard deviation for each measurement. 
+
+test_train1 <- test_train1[grep("subject|activity|mean|std", colnames(test_train1), value = TRUE)]
+dim(test_train1)
+
+# 17) From the data set in step 4 (in this script is the dataset 
+# in step 16) ), creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+prog_ass <- aggregate(test_train1[, 3:81], list(test_train1$activity, test_train1$subject), mean)
+
+rm(test_train1)
+
+# 18) finished! 
+
 q()
